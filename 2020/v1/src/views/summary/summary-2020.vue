fef066428a19250db3c3bc12e43c798696b993bc
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-01-01 00:46:04 +0800

    1.新功能:增加2020年总结

--- 2020/v1/src/views/summary/summary-2020.vue
+++ 2020/v1/src/views/summary/summary-2020.vue
@@ -1,39 +1,35 @@
 <template>
-    <swiper class="swiper" :options="swiperOption">
+    <swiper class="swiper" ref="mySwiper" :options="swiperOptions">
         <swiper-slide>Slide 1</swiper-slide>
         <swiper-slide>Slide 2</swiper-slide>
         <swiper-slide>Slide 3</swiper-slide>
         <swiper-slide>Slide 4</swiper-slide>
         <swiper-slide>Slide 5</swiper-slide>
-        <swiper-slide>Slide 6</swiper-slide>
-        <swiper-slide>Slide 7</swiper-slide>
-        <swiper-slide>Slide 8</swiper-slide>
-        <swiper-slide>Slide 9</swiper-slide>
-        <swiper-slide>Slide 10</swiper-slide>
         <div class="swiper-pagination" slot="pagination"></div>
     </swiper>
 </template>
 
 <script>
-    import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
-    import 'swiper/swiper-bundle.css'
-
     export default {
-        name: 'summary-2020',
-        title: 'Summary 2020',
-        components: {
-            Swiper,
-            SwiperSlide
-        },
+        name: 'carrousel',
         data() {
             return {
-                swiperOption: {
+                swiperOptions: {
                     pagination: {
-                        el: '.swiper-pagination',
-                        dynamicBullets: true
-                    }
+                        el: '.swiper-pagination'
+                    },
+                    // Some Swiper option/callback...
                 }
             }
+        },
+        computed: {
+            swiper() {
+                return this.$refs.mySwiper.$swiper
+            }
+        },
+        mounted() {
+            console.log('Current Swiper instance object', this.swiper)
+            this.swiper.slideTo(3, 1000, false)
         }
     }
 </script>
